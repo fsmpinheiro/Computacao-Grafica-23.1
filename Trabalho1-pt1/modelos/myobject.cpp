c48be8500baeccb7184cdee5d80214a837a1b84a
Samuel Pinheiro <31665870+fsmpinheiro@users.noreply.github.com>
2025-02-23 21:41:03 -0300

    Update classe myObject
    Atualização para rotação fazendo uso de quaternions

--- Trabalho1-pt1/modelos/myobject.cpp
+++ Trabalho1-pt1/modelos/myobject.cpp
@@ -7,9 +7,18 @@ MyObject::MyObject( int k ){
 void MyObject::desenha( ){
 
     glTranslatef( trans.x, trans.y, trans.z );
-    glRotatef( rotat.z, 0, 0, 1);
-    glRotatef( rotat.y, 0, 1, 0);
-    glRotatef( rotat.x, 1, 0, 0);
+
+    if( glutGUI::use_quatern ){
+        GUI::quaternionRotationf(rotat.z, 0, 0, 1 );
+        GUI::quaternionRotationf( rotat.y, 0, 1, 0 );
+        GUI::quaternionRotationf( rotat.x, 1, 0, 0 );
+    } else {
+        glRotatef( rotat.z, 0, 0, 1);
+        glRotatef( rotat.y, 0, 1, 0);
+        glRotatef( rotat.x, 1, 0, 0);
+    }
+
+
 
 //    if( origin ){
 //        GUI::drawOrigin( 0.5 );
